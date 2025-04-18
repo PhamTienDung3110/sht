9058753c7135f924903a147d9fc0905c2f31d520
Tandt <tandt@runsystem.net>
2025-04-18 10:57:16 +0700

    update

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
                 </svg>
               </Link> */}
 
-              <Link
+              {/* <Link
                 href="/news"
                 className={`p-2 rounded-lg ${isActive('/news') ? 'bg-gray-100 text-[#3d82af]' : 'hover:bg-gray-100 text-black'}`}
               >
@@ -64,7 +64,7 @@ const Header = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                 </svg>
-              </Link>
+              </Link> */}
 
               {/* <Link
                 href="/chat"
@@ -84,14 +84,14 @@ const Header = () => {
                 </svg>
               </Link> */}
 
-              <Link
+              {/* <Link
                 href="/documents"
                 className={`p-2 rounded-lg ${isActive('/documents') ? 'bg-gray-100 text-[#3d82af]' : 'hover:bg-gray-100 text-black'}`}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
-              </Link>
+              </Link> */}
 
               {/* <Link
                 href="/search"
